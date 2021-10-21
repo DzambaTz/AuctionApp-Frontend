57c9536053bf7a3a3c1fdd74b5c0c417f954a512
DzambaTz <tarik.dzambic@gmail.com>
2021-10-21 14:56:36 +0200

    Created Privacy policy and Terms and Conditions page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import LoginPage from "./Pages/login-page";
 import RegistrationPage from "./Pages/registration-page";
 import PageNotFound from "./Pages/404-page";
 import AboutUsPage from "./Pages/about";
+import PrivacyPage from "./Pages/privacy-policy";
+import TermsPage from "./Pages/terms-and-conditions";
 
 function App() {
-  return(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={LandingPage}/>
-      <Route exact path="/login" component={LoginPage}/>
-      <Route exact path="/signup" component={RegistrationPage}/>
-      <Route exact path="/about" component={AboutUsPage} />
-      <Route component={PageNotFound} />
-    </Switch>
-  </Router>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={RegistrationPage} />
+        <Route exact path="/about" component={AboutUsPage} />
+        <Route exact path="/privacy" component={PrivacyPage} />
+        <Route exact path="/terms" component={TermsPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
   );
 }
 
