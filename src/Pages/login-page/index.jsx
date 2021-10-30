c7b39e675333ec904a05a64657b842cf71b20dd7
DzambaTz <tarik.dzambic@gmail.com>
2021-10-30 14:27:57 +0200

    Connected login and registration forms to REST API

--- src/Pages/login-page/index.jsx
+++ src/Pages/login-page/index.jsx
@@ -1,49 +1,117 @@
-import React from "react";
+import React, { Component } from "react";
 import NavbarBlack from "../../Components/navbar-black";
 import NavbarWhite from "../../Components/navbar-white";
 import Footer from "../../Components/footer";
+import AuthService from "../../Auth/auth.service";
 
 import "./index.scss";
 
-const LoginPage = () => {
-  return (
-    <div>
-      <NavbarBlack />
-      <NavbarWhite />
-      <div className="login-banner">Login</div>
-      <form className="login-form">
-        <h1>LOGIN</h1>
-        <label htmlFor="login-email">Enter Email</label>
-        <br />
-        <input
-          type="email"
-          name="login-email"
-          id="login-email"
-          placeholder="Email"
-          required
-        />
-        <br />
-        <label htmlFor="login-password">Password</label>
-        <br />
-        <input
-          type="password"
-          name="login-password"
-          id="login-password"
-          placeholder="Password"
-          Style="margin-bottom: 5px;"
-          required
-        />
-        <br />
-        <input type="checkbox" name="login-remember" id="login-remember" />
-        <label htmlFor="login-remember" Style="margin-left: 5px">
-          Remember me
-        </label>{" "}
-        <br />
-        <button type="submit">LOGIN</button>
-      </form>
-      <Footer />
-    </div>
-  );
-};
+export default class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
 
-export default LoginPage;
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      message: "",
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+
+    this.setState({
+      message: "",
+      loading: true,
+    });
+
+    AuthService.login(this.state.email, this.state.password).then(
+      () => {
+        this.props.history.push("/");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        this.setState({
+          loading: false,
+          message: resMessage,
+        });
+      }
+    );
+
+    this.setState({
+      loading: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <NavbarBlack />
+        <NavbarWhite />
+        <div className="login-banner">Login</div>
+        <form className="login-form" onSubmit={this.handleLogin}>
+          <h1>LOGIN</h1>
+          <label htmlFor="login-email">Enter Email</label>
+          <br />
+          <input
+            type="email"
+            name="login-email"
+            id="login-email"
+            autoComplete="off"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.onChangeEmail}
+            required
+          />
+          <br />
+          <label htmlFor="login-password">Password</label>
+          <br />
+          <input
+            type="password"
+            name="login-password"
+            id="login-password"
+            placeholder="Password"
+            Style="margin-bottom: 5px;"
+            value={this.state.password}
+            onChange={(this, this.onChangePassword)}
+            required
+          />
+          <br />
+          <input type="checkbox" name="login-remember" id="login-remember" />
+          <label htmlFor="login-remember" Style="margin-left: 5px">
+            Remember me
+          </label>{" "}
+          <br />
+          <button type="submit">LOGIN</button>
+          {this.state.message && (
+            <div className="login-message">{this.state.message}</div>
+          )}
+        </form>
+        <Footer />
+      </div>
+    );
+  }
+}
--- src/Pages/registration-page/index.jsx
+++ src/Pages/registration-page/index.jsx
@@ -1,65 +1,162 @@
-import React from "react";
+import React, { Component } from "react";
 import NavbarBlack from "../../Components/navbar-black";
 import NavbarWhite from "../../Components/navbar-white";
 import Footer from "../../Components/footer";
+import AuthService from "../../Auth/auth.service";
 
 import "./index.scss";
 
-const RegistrationPage = () => {
-  return (
-    <div>
-      <NavbarBlack />
-      <NavbarWhite />
-      <div className="registration-banner">Registration</div>
-      <form className="registration-form">
-        <h1>REGISTER</h1>
-        <label htmlFor="registration-first-name">First name</label>
-        <br />
-        <input
-          type="text"
-          name="registration-first-name"
-          id="registration-first-name"
-          placeholder="First name"
-          autoComplete="off"
-          required
-        />
-        <br />
-        <label htmlFor="registration-last-name">Last name</label>
-        <br />
-        <input
-          type="text"
-          name="registration-last-name"
-          id="registration-last-name"
-          placeholder="Last name"
-          autoComplete="off"
-          required
-        />
-        <label htmlFor="registration-email">Email</label>
-        <input
-          type="email"
-          name="registration-email"
-          id="registration-email"
-          placeholder="Email"
-          autoComplete="off"
-          required
-        />
-        <label htmlFor="registration-password">Password</label>
-        <input
-          type="password"
-          name="registration-password"
-          id="registration-password"
-          placeholder="Password"
-          required
-        />
-        <button type="submit">REGISTER</button>
-        <br />
-        <div>
-          Already have an account? <a href="/login">Login</a>
-        </div>
-      </form>
-      <Footer />
-    </div>
-  );
-};
+export default class RegisterPage extends Component {
+  constructor(props) {
+    super(props);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.onChangeFirst_name = this.onChangeFirst_name.bind(this);
+    this.onChangeLast_name = this.onChangeLast_name.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
 
-export default RegistrationPage;
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      successful: false,
+      message: "",
+    };
+  }
+  onChangeFirst_name(e) {
+    this.setState({
+      first_name: e.target.value,
+    });
+  }
+
+  onChangeLast_name(e) {
+    this.setState({
+      last_name: e.target.value,
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleRegister(e) {
+    e.preventDefault();
+    this.setState({
+      message: "",
+      successful: false,
+    });
+
+    AuthService.register(
+      this.state.first_name,
+      this.state.last_name,
+      this.state.email,
+      this.state.password
+    ).then(
+      (reponse) => {
+        this.setState({
+          message: reponse.data.message,
+          successful: true,
+        });
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        this.setState({
+          successful: false,
+          message: resMessage,
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <NavbarBlack />
+        <NavbarWhite />
+        <div className="registration-banner">Registration</div>
+        <form className="registration-form" onSubmit={this.handleRegister}>
+          <h1>REGISTER</h1>
+          <label htmlFor="registration-first-name">First name</label>
+          <br />
+          <input
+            type="text"
+            name="registration-first-name"
+            id="registration-first-name"
+            placeholder="First name"
+            autoComplete="off"
+            value={this.state.first_name}
+            onChange={this.onChangeFirst_name}
+            required
+          />
+          <br />
+          <label htmlFor="registration-last-name">Last name</label>
+          <br />
+          <input
+            type="text"
+            name="registration-last-name"
+            id="registration-last-name"
+            placeholder="Last name"
+            autoComplete="off"
+            value={this.state.last_name}
+            onChange={this.onChangeLast_name}
+            required
+          />
+          <label htmlFor="registration-email">Email</label>
+          <input
+            type="email"
+            name="registration-email"
+            id="registration-email"
+            placeholder="Email"
+            autoComplete="off"
+            value={this.state.email}
+            onChange={this.onChangeEmail}
+            required
+          />
+          <label htmlFor="registration-password">Password</label>
+          <input
+            type="password"
+            name="registration-password"
+            id="registration-password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.onChangePassword}
+            required
+          />
+          <button type="submit">REGISTER</button>
+          <br />
+          {this.state.message && (
+            <div
+              Style="color: #252525"
+              className={
+                this.state.successful
+                  ? "registration-message successful"
+                  : "registration-message failed"
+              }
+            >
+              {this.state.message}
+            </div>
+          )}
+          <div>
+            Already have an account? <a href="/login">Login</a>
+          </div>
+        </form>
+        <Footer />
+      </div>
+    );
+  }
+}
