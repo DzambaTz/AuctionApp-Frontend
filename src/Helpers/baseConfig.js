13ff81faddc08bcd0b5fff0bd650ccd49364d0f2
DzambaTz <tarik.dzambic@gmail.com>
2021-11-26 18:33:21 +0100

    Fixed login error message

--- src/Helpers/baseConfig.js
+++ src/Helpers/baseConfig.js
@@ -1,7 +1,7 @@
 //Production
-const BASE_URL = "https://nameless-inlet-82657.herokuapp.com/api/";
+//const BASE_URL = "https://nameless-inlet-82657.herokuapp.com/api/";
 
 //Dev
-//const BASE_URL = "http://localhost:8080/api/";
+const BASE_URL = "http://localhost:8080/api/";
 
 export { BASE_URL };
