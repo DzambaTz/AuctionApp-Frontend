5cdb6344c62c1b10e5e537c70c71d9498ff70833
DzambaTz <tarik.dzambic@gmail.com>
2021-12-03 23:33:10 +0100

    Ticket 8 - Shop page: Implement all filters

--- src/Pages/shop-page/index.jsx
+++ src/Pages/shop-page/index.jsx
@@ -13,6 +13,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import testData from "../../Helpers/test-data";
 import statusCodes from "../../Helpers/status-codes";
+import MultiRangeSlider from "../../Components/multi-range-slider";
+import { act } from "react-dom/test-utils";
 
 function titleCase(string) {
   return string[0].toUpperCase() + string.slice(1).toLowerCase();
@@ -20,20 +22,41 @@ function titleCase(string) {
 
 function ShopPage() {
   const CATEGORY = "Category";
+  const SUBCATEGORY = "Subcategory";
+  const PRICE_RANGE = "Price range";
+
+  const pathCategory = useParams().category;
 
   const [newArrivals, setNewArrivals] = useState("");
   const [filteredItems, setFilteredItems] = useState(newArrivals);
-  const [category, setCategory] = useState([titleCase(useParams().category)]);
+  const [category, setCategory] = useState(
+    pathCategory ? [titleCase(pathCategory)] : []
+  );
   const [activeFilters, setActiveFilters] = useState([]);
+  const [expandedCategories, setExpandedCategories] = useState([]);
+  const [subcategory, setSubcategory] = useState([]);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [minPriceSlider, setMinPriceSlider] = useState(0);
+  const [maxPriceSlider, setMaxPriceSlider] = useState(0);
 
   useEffect(() => {
     itemService.getNewArrivals().then((response) => {
       if (response.status == statusCodes.OK) setNewArrivals(response.body);
+      setMinPrice(
+        response.body.reduce((prev, curr) =>
+          prev.startPrice < curr.startPrice ? prev : curr
+        ).startPrice
+      );
+      setMaxPrice(
+        response.body.reduce((prev, curr) =>
+          prev.startPrice > curr.startPrice ? prev : curr
+        ).startPrice
+      );
     });
   }, []);
 
   useEffect(() => {
-    console.log(process.env.BASE_URL);
     if (newArrivals && category.length != 0) {
       setFilteredItems(
         newArrivals.filter((item) => {
@@ -56,6 +79,90 @@ function ShopPage() {
     }
   }, [newArrivals, category]);
 
+  useEffect(() => {
+    if (subcategory.length != 0) {
+      let newFilters = [];
+      subcategory.map((subcat) => {
+        if (
+          !activeFilters.some(
+            (filter) => filter.title === SUBCATEGORY && filter.value === subcat
+          )
+        ) {
+          newFilters.push({ title: SUBCATEGORY, value: subcat });
+        }
+      });
+      setActiveFilters(activeFilters.concat(newFilters));
+    }
+  }, [subcategory]);
+
+  useEffect(() => {
+    if (newArrivals && activeFilters.length != 0) {
+      setFilteredItems(
+        newArrivals.filter((item) => {
+          if (activeFilters.some((filter) => filter.title == PRICE_RANGE)) {
+            if (
+              item.startPrice >= minPriceSlider &&
+              item.startPrice <= maxPriceSlider
+            ) {
+              if (
+                category.includes(item.category) ||
+                subcategory.includes(item.category + "/" + item.subcategory)
+              ) {
+                return true;
+              } else {
+                if (category.length == 0 && subcategory.length == 0) {
+                  return true;
+                }
+              }
+            } else {
+              return false;
+            }
+          } else {
+            if (
+              category.includes(item.category) ||
+              subcategory.includes(item.category + "/" + item.subcategory)
+            ) {
+              return true;
+            } else {
+              if (category.length == 0 && subcategory.length == 0) {
+                return true;
+              }
+            }
+          }
+        })
+      );
+    } else {
+      setFilteredItems(newArrivals);
+    }
+  }, [activeFilters]);
+
+  useEffect(() => {
+    if (!activeFilters.some((filter) => filter.title === PRICE_RANGE)) {
+      setActiveFilters(
+        activeFilters.concat({
+          title: PRICE_RANGE,
+          min: minPriceSlider,
+          max: maxPriceSlider,
+          value: "$" + minPriceSlider + "-$" + maxPriceSlider,
+        })
+      );
+    } else {
+      let priceFilter = activeFilters.find(
+        (filter) => filter.title === PRICE_RANGE
+      );
+      let newActiveFilters = activeFilters.filter(
+        (filter) => filter.title !== PRICE_RANGE
+      );
+      priceFilter = {
+        title: PRICE_RANGE,
+        min: minPriceSlider,
+        max: maxPriceSlider,
+        value: "$" + minPriceSlider + "-$" + maxPriceSlider,
+      };
+      setActiveFilters(newActiveFilters.concat(priceFilter));
+    }
+  }, [minPriceSlider, maxPriceSlider]);
+
   const removeFilter = (title, value) => {
     setActiveFilters(
       activeFilters.filter((filter) => {
@@ -65,11 +172,51 @@ function ShopPage() {
     if (title == CATEGORY) {
       setCategory(category.filter((cat) => cat !== value));
     }
+    if (title == SUBCATEGORY) {
+      setSubcategory(subcategory.filter((subcat) => subcat !== value));
+      document.getElementById(value).checked = false;
+    }
   };
 
-  const addOrRemoveCategoryFilter = (newCategory) => {
+  const addCategoryFilter = (newCategory) => {
     if (!category.includes(newCategory))
       setCategory(category.concat(newCategory));
+  };
+
+  const expandOrCollapseCategory = (categoryName, e) => {
+    console.log(minPrice, maxPrice);
+    if (e.target.innerText == "+") {
+      setExpandedCategories(expandedCategories.concat(categoryName));
+    } else {
+      setExpandedCategories(
+        expandedCategories.filter((cat) => cat !== categoryName)
+      );
+    }
+    e.target.innerText = e.target.innerText == "+" ? "–" : "+";
+  };
+
+  const addOrRemoveSubcategoryFilter = (categoryName, subcategoryName, e) => {
+    if (e.target.checked) {
+      setSubcategory(subcategory.concat(categoryName + "/" + subcategoryName));
+    } else {
+      setSubcategory(
+        subcategory.filter(
+          (subcat) => subcat !== categoryName + "/" + subcategoryName
+        )
+      );
+      setActiveFilters(
+        activeFilters.filter(
+          (filter) =>
+            filter.title !== SUBCATEGORY ||
+            filter.value !== categoryName + "/" + subcategoryName
+        )
+      );
+    }
+  };
+
+  const changePriceRange = (min, max) => {
+    setMinPriceSlider(min);
+    setMaxPriceSlider(max);
   };
 
   return (
@@ -82,18 +229,85 @@ function ShopPage() {
           {testData.categories.map((category) => {
             return (
               <div>
-                <h2>{category.name}</h2>{" "}
-                <button
+                <h2
                   onClick={() => {
-                    addOrRemoveCategoryFilter(category.name);
+                    addCategoryFilter(category.name);
+                  }}
+                >
+                  {category.name}
+                </h2>{" "}
+                <button
+                  onClick={(e) => {
+                    expandOrCollapseCategory(category.name, e);
                   }}
                 >
                   +
                 </button>
+                {expandedCategories.includes(category.name) &&
+                  category.subcategories.map((subcat) => {
+                    return (
+                      <div className="subcategory">
+                        <input
+                          onChange={(e) =>
+                            addOrRemoveSubcategoryFilter(
+                              category.name,
+                              subcat,
+                              e
+                            )
+                          }
+                          type="checkbox"
+                          id={category.name + "/" + subcat}
+                        />
+                        <div Style="padding-top: 2px">
+                          {subcat} (
+                          {
+                            newArrivals.filter(
+                              (item) =>
+                                item.category == category.name &&
+                                item.subcategory == subcat
+                            ).length
+                          }
+                          )
+                        </div>
+                      </div>
+                    );
+                  })}
               </div>
             );
           })}
         </div>
+        {minPrice > 0 && maxPrice > 0 && (
+          <div className="price-filter">
+            <h1>Price Range</h1>
+            <input
+              type="text"
+              className="price-input"
+              Style="margin-right: 14%;"
+              value={"$" + minPriceSlider}
+            />
+            <input
+              type="text"
+              className="price-input"
+              value={"$" + maxPriceSlider}
+            />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <MultiRangeSlider
+              min={minPrice}
+              max={maxPrice}
+              onChange={({ min, max }) => changePriceRange(min, max)}
+            />
+            <h2>
+              ${minPriceSlider}-${maxPriceSlider}
+            </h2>
+            <h3>
+              The average price is ${Math.floor((minPrice + maxPrice) / 2)}
+            </h3>
+          </div>
+        )}
       </div>
       <div className="product-list">
         <div className="active-filters">
